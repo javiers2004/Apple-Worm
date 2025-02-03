6b314376cdc4914b902e5f5fe43bb58c1701400b
js834149 <js834149@gmail.com>
2025-02-03 12:55:24 +0100

    portal animado

--- main.py
+++ main.py
@@ -74,6 +74,23 @@ niveles = [
         ],
         "serpiente_inicio": [[6, 3], [5, 3], [4, 3]],
         "longitud_inicio": 3
+    },
+    {
+        "mapa": [
+            "00000000000000000000",            
+            "00000000000000000000",
+            "00000000000000000000",
+            "00000000000000000000",
+            "00000000000000000000",
+            "000000000??000000000",
+            "000000000??000000000",
+            "0000000######0000000",
+            "00000000000000000000",
+            "000000000000@0000000",
+            "00000000000000000000"
+        ],
+        "serpiente_inicio": [[7, 6]],
+        "longitud_inicio": 1
     }
 ]
 
@@ -86,6 +103,18 @@ imagen_manzana = pygame.image.load("manzana.png")
 imagen_manzana = pygame.transform.scale(imagen_manzana, (TAMANO_CELDA, TAMANO_CELDA))
 imagen_bloque = pygame.image.load("bloque.png")
 imagen_bloque = pygame.transform.scale(imagen_bloque, (TAMANO_CELDA, TAMANO_CELDA))
+
+fotogramas_portal = []
+num_fotogramas = 5  
+
+for i in range(num_fotogramas):
+    imagen = pygame.image.load(f"portal\portalFrame{i+1}.png")
+    imagen = pygame.transform.scale(imagen, (TAMANO_CELDA, TAMANO_CELDA))  # Ajusta al tamaño de las celdas
+    fotogramas_portal.append(imagen)
+
+indice_fotograma_portal = 0
+reloj_portal = pygame.time.get_ticks()  
+duracion_fotograma_portal = 100
 
 # Inicializar ventana
 ventana = pygame.display.set_mode((ANCHO * TAMANO_CELDA, ALTO * TAMANO_CELDA), pygame.FULLSCREEN)
@@ -119,7 +148,7 @@ def dibujar_mapa():
                 elif mapa[y][x] == "manzana":
                     ventana.blit(imagen_manzana, (x * TAMANO_CELDA, y * TAMANO_CELDA))
                 elif mapa[y][x] == "portal":
-                    pygame.draw.rect(ventana, COLORES["portal"], (x * TAMANO_CELDA, y * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA))
+                    dibujar_portal_animado(x, y)
 
 def dibujar_serpiente(serpiente):
     for i, segmento in enumerate(serpiente):
@@ -127,6 +156,15 @@ def dibujar_serpiente(serpiente):
             pygame.draw.rect(ventana, (0, 200, 0), (segmento[0] * TAMANO_CELDA, segmento[1] * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA))  # Color más oscuro
         else:
             pygame.draw.rect(ventana, COLORES["serpiente"], (segmento[0] * TAMANO_CELDA, segmento[1] * TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA))
+
+def dibujar_portal_animado(x, y):
+    global indice_fotograma_portal, reloj_portal
+    tiempo_actual = pygame.time.get_ticks()
+    if tiempo_actual - reloj_portal > duracion_fotograma_portal:
+        indice_fotograma_portal = (indice_fotograma_portal + 1) % len(fotogramas_portal)
+        reloj_portal = tiempo_actual
+
+    ventana.blit(fotogramas_portal[indice_fotograma_portal], (x * TAMANO_CELDA, y * TAMANO_CELDA))
 
 def mover_serpiente(serpiente, direccion):
     global mapa
@@ -167,17 +205,19 @@ def verificar_gravedad(serpiente, mapa):
     while caer == True:
         for i, segmento in enumerate(serpiente):
             x, y = segmento
-            if mapa[y + 1][x] == "bloque" or mapa[y + 1][x] == "manzana":
+            # Verificar que y + 1 esté dentro del rango del mapa
+            if y + 1 >= len(mapa) or mapa[y + 1][x] == "bloque" or mapa[y + 1][x] == "manzana":
                 caer = False
         if caer == True:
             for i, segmento in enumerate(serpiente):
                 serpiente[i][1] += 1 
             pygame.display.update()
             reloj.tick(10)
-            pygame.time.wait(50) 
+            pygame.time.wait(50)
             ventana.fill(COLORES["fondo"])
             dibujar_mapa()
             dibujar_serpiente(serpiente)
+
 
         
 
